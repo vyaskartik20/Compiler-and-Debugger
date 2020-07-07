b5af97ac6f8b5b61d6c4d104af1906ac803d8aa7
vyaskartik20 <Kartik Vyas>
2020-07-07 16:03:40 +0530

    App.js modified

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and be cool
+          Learn React 
         </a>
       </header>
     </div>
